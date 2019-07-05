61decf05f428eddf18894e613a222f320b856b7b
D812359 <287202628@qq.com>
2019-07-05 17:39:20 +0800

    正则表达式

---
+++ java-parent/java-senior/src/main/java/com/atu/senior/Regular/Test.java
@@ -0,0 +1,20 @@
+package com.atu.senior.Regular;
+
+/**
+ * @Author: ta0567
+ * @Date: 2019/7/5 17:37
+ * @Description: 正则表达式
+ */
+public class Test {
+    public static void main(String[] args) {
+        String str = "4092x1863x1461mm";
+        String formatDate = str.substring(0, str.length() - 2).toUpperCase();
+        System.out.println(formatDate);
+        String regex = "^[0-9]{4}[X][0-9]{4}[X][0-9]{4}+$";
+
+        System.out.println(formatDate.matches(regex));
+        String[] lwh = formatDate.split("X");
+
+        System.out.println(lwh[0] + "-----" + lwh[1] + "----------" + lwh[2]);
+    }
+}
