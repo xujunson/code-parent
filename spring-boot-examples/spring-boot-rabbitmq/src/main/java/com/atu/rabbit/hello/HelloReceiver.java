f531265d190f1446ab801bf535c2642f3f98443a
zone <287202628@qq.com>
2019-10-23 17:50:43 +0800

    spring-boot-rabbitmq

---
+++ spring-boot-examples/spring-boot-rabbitmq/src/main/java/com/atu/rabbit/hello/HelloReceiver.java
@@ -0,0 +1,19 @@
+package com.atu.rabbit.hello;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ：mark
+ * @date ：Created in 2019/10/23 16:56
+ * @description：接受者
+ */
+@Component
+@RabbitListener(queues = "hello")
+public class HelloReceiver {
+    @RabbitHandler
+    public void process(String hello) {
+        System.out.println("Receiver  : " + hello);
+    }
+}
