c9e45255323d607e089238d6aad3d4feb1364178
zone <287202628@qq.com>
2019-10-23 09:54:37 +0800

    spring-boot-redis

--- spring-boot-examples/spring-boot-redis/src/main/java/com/atu/web/UserController.java
+++ spring-boot-examples/spring-boot-redis/src/main/java/com/atu/web/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
 /**
  * @author ：mark
  * @date ：Created in 2019/10/21 17:42
@@ -18,5 +21,15 @@ public class UserController {
         User user=new User("aa@126.com", "aa", "aa123456", "aa","123");
         System.out.println("success");
         return user;
+    }
+
+    @RequestMapping("/uid")
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return session.getId();
     }
 }
