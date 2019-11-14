772752dccc1b17d70021361bffd3e665952a2e58
zone <287202628@qq.com>
2019-11-14 16:41:32 +0800

    spring-cloud-code

---
+++ spring-cloud-examples/gateway-service-zuul/gateway-service-zuul-simple/src/main/java/com/atu/GatewayServiceZuulSimpleApplication.java
@@ -0,0 +1,15 @@
+package com.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayServiceZuulSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceZuulSimpleApplication.class, args);
+    }
+
+}
