7bb3f268db614d5aec40caea7af80df63178c334
D812359 <287202628@qq.com>
2020-03-10 19:14:07 +0800

    线程八大核心-4、图解线程生命周期 5、趣解Thread和Object类中线程相关方法-wait方法

---
+++ java-parent/java-high-concurrency/java-high-concurrency-core/src/main/java/com/atu/sixstates/BlockedWaitingTimedWaiting.java
@@ -0,0 +1,46 @@
+package com.atu.sixstates;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-10 15:25
+ * @description: 展示 Blocked、Waiting、TimedWaiting
+ */
+public class BlockedWaitingTimedWaiting implements Runnable {
+    public static void main(String[] args) {
+        BlockedWaitingTimedWaiting runnable = new BlockedWaitingTimedWaiting();
+        Thread thread1 = new Thread(runnable);
+        thread1.start();
+        Thread thread2 = new Thread(runnable);
+        thread2.start();
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //打印出TIMED_WAITING状态，因为正在执行 Thread.sleep(1000);
+        System.out.println("thread1: " + thread1.getState());
+        //打印出BLOCKED状态，因为thread2想拿到syn()的锁却拿不到
+        System.out.println("thread2: " + thread2.getState());
+        try {
+            Thread.sleep(1300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //WAITING
+        System.out.println("thread1: " + thread1.getState());
+    }
+
+    @Override
+    public void run() {
+        syn();
+    }
+
+    private synchronized void syn() {
+        try {
+            Thread.sleep(1000);
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
