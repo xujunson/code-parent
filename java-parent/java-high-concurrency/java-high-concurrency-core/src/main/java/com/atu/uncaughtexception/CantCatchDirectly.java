cef20841411df0888ee1f1e446286fb83c70a565
D812359 <287202628@qq.com>
2020-03-11 11:29:47 +0800

    线程八大核心————线程异常处理知多少？

---
+++ java-parent/java-high-concurrency/java-high-concurrency-core/src/main/java/com/atu/uncaughtexception/CantCatchDirectly.java
@@ -0,0 +1,36 @@
+package com.atu.uncaughtexception;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-11 10:05
+ * @description: 1、不加try catch 抛出4个异常，都带线程名字
+ * 2、加了try catch 期望捕获到第一个线程的异常，线程 2 3 4不应该运行，希望看到打印出Caught Exception
+ * 3、执行发现，根本没有Caught Exception，线程2 3 4依然运行并抛出异常
+ * 说明线程的异常不能用传统方法捕获
+ */
+public class CantCatchDirectly implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            new Thread(new CantCatchDirectly(), "MyThread-1").start();
+            Thread.sleep(300);
+            new Thread(new CantCatchDirectly(), "MyThread-2").start();
+            Thread.sleep(300);
+            new Thread(new CantCatchDirectly(), "MyThread-3").start();
+            Thread.sleep(300);
+            new Thread(new CantCatchDirectly(), "MyThread-4").start();
+
+        } catch (RuntimeException e) {
+            System.out.println("Caught Exception.");
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException e) {
+            System.out.println("Caught Exception.");
+        }
+
+    }
+}
---
+++ java-parent/java-high-concurrency/java-high-concurrency-core/src/main/java/com/atu/uncaughtexception/ExceptionInChildThread.java
@@ -0,0 +1,21 @@
+package com.atu.uncaughtexception;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-11 10:01
+ * @description: 单线程时，抛出，处理，有异常堆栈
+ * 但是多线程时，子线程发生异常会什么不同？
+ */
+public class ExceptionInChildThread implements Runnable {
+    public static void main(String[] args) {
+        new Thread(new ExceptionInChildThread()).start();
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+
+    @Override
+    public void run() {
+        throw new RuntimeException();
+    }
+}
