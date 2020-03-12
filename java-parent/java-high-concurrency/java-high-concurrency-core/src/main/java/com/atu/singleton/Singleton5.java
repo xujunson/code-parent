8d258bb2bda81efdeee46f222422a068df967280
D812359 <287202628@qq.com>
2020-03-12 22:39:32 +0800

    深入浅出并发编程底层原理-下篇————单例模式

---
+++ java-parent/java-high-concurrency/java-high-concurrency-core/src/main/java/com/atu/singleton/Singleton5.java
@@ -0,0 +1,24 @@
+package com.atu.singleton;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-12 22:31
+ * @description: 懒汉式(线程不安全) 不推荐
+ */
+public class Singleton5 {
+    //懒汉————只有在需要用的时候才加载
+    private static Singleton5 instance;
+
+    private Singleton5() {
+    }
+
+    //线程不安全 创建多个实例
+    public static Singleton5 getInstance() {
+        if (instance == null) {
+            synchronized (Singleton5.class) {
+                instance = new Singleton5();
+            }
+        }
+        return instance;
+    }
+}
