c56d1b6980d73800245b4560a1853826965cb171
D812359 <287202628@qq.com>
2020-03-13 23:22:09 +0800

    并发死锁问题与企业级解决方案————活锁

---
+++ java-parent/java-high-concurrency/java-high-concurrency-core/src/main/java/com/atu/deadlock/LiveLock.java
@@ -0,0 +1,85 @@
+package com.atu.deadlock;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-13 21:52
+ * @description: 演示活锁问题
+ */
+public class LiveLock {
+    static class Spoon {
+        private Diner owner; //就餐者
+
+        public synchronized void use() {
+            System.out.printf("%s has eaten!", owner.name);
+        }
+
+        public Spoon(Diner owner) {
+            this.owner = owner;
+        }
+
+        public Diner getOwner() {
+            return owner;
+        }
+
+        public void setOwner(Diner owner) {
+            this.owner = owner;
+        }
+
+    }
+
+    static class Diner {
+        private String name;
+        private boolean isHungry;
+
+        public Diner(String name) {
+            this.name = name;
+            isHungry = true;
+        }
+
+        public void eatWith(Spoon spoon, Diner spouse) {
+            while (isHungry) { //只有饿的情况下才能进来
+                if (spoon.owner != this) { //没有勺子判断
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    continue;
+                }
+                //问题在此处：一直再谦让
+                if (spouse.isHungry) {
+                    System.out.println(name + ": 亲爱的" + spouse.name + "你先吃把");
+                    spoon.setOwner(spouse);
+                    continue;
+                }
+
+                spoon.use();
+                isHungry = false;
+                System.out.println(name + ": 我吃好了");
+                spoon.setOwner(spouse);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Diner husband = new Diner("牛郎");
+        Diner wife = new Diner("织女");
+
+        Spoon spoon = new Spoon(husband);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                husband.eatWith(spoon, wife);
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                wife.eatWith(spoon, husband);
+            }
+        }).start();
+    }
+}
+
+
