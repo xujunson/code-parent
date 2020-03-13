afd732a68bdfd42db2e01bfbc6fa2fea8ecf9c4d
D812359 <287202628@qq.com>
2020-03-13 11:34:46 +0800

    深入浅出并发编程底层原理-下篇————完结

---
+++ java-parent/java-high-concurrency/java-high-concurrency-core/src/main/java/com/atu/singleton/Singleton8.java
@@ -0,0 +1,15 @@
+package com.atu.singleton;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-12 22:31
+ * @description: 枚举单例
+ */
+public enum  Singleton8 {
+    INSTANCE;
+
+    public void whatever(){
+
+    }
+}
+
