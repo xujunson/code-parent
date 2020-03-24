883da1bc30c45c08f0069bab594d75ef904e543e
D812359 <287202628@qq.com>
2020-03-24 12:43:57 +0800

    AQS【进阶必备，并发灵魂人物】

--- java-parent/java-high-concurrency/java-high-concurrency-tools/src/main/java/com/atu/aqs/OneShotLatch.java
+++ java-parent/java-high-concurrency/java-high-concurrency-tools/src/main/java/com/atu/aqs/OneShotLatch.java
@@ -6,16 +6,20 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
  * @author: Tom
  * @date: 2020-03-24 11:54
  * @description: 自己用AQS实现一个简单的线程协作器
+ * 起初门闩是关闭的，那么会有很多线程调用await方法，因为门闩是关闭的，谁调用谁就陷入等待状态；
+ * 直到后续有一个线程调用signal方法——相当于把门闩打开，此时之前所有的线程都会被释放；
+ * 使用state表示门是不是被打开，0——关闭，1——打开
  */
 public class OneShotLatch {
     private final Sync sync = new Sync();
 
-    //获取
+    //共享锁
+    //获取——含义和CountDownLatch一样
     public void await() {
-        sync.acquireShared(0);
+        sync.acquireShared(0); //尝试获取锁
     }
 
-    //释放
+    //释放——同countDown()
     public void signal() {
         sync.releaseShared(0);
     }
@@ -42,6 +46,7 @@ public class OneShotLatch {
                 @Override
                 public void run() {
                     System.out.println(Thread.currentThread().getName() + "尝试获取latch，获取失败那就等待");
+
                     oneShotLatch.await();
                     System.out.println("开闸放行" + Thread.currentThread().getName() + "继续运行");
                 }
@@ -52,9 +57,9 @@ public class OneShotLatch {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName()+"尝试获取latch，获取失败那就等待");
+                System.out.println(Thread.currentThread().getName() + "尝试获取latch，获取失败那就等待");
                 oneShotLatch.await();
-                System.out.println("开闸放行"+Thread.currentThread().getName()+"继续运行");
+                System.out.println("开闸放行" + Thread.currentThread().getName() + "继续运行");
             }
         }).start();
     }
