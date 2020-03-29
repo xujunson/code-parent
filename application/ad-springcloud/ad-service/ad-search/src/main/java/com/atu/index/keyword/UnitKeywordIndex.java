f928400f2d0a3cc033c9246c541e4be05718831a
D812359 <287202628@qq.com>
2020-03-29 11:06:16 +0800

    广告检索系统-广告数据索引的设计与实现【难度在逐步上升】 3、关键词索引对象定义与服务实现

---
+++ application/ad-springcloud/ad-service/ad-search/src/main/java/com/atu/index/keyword/UnitKeywordIndex.java
@@ -0,0 +1,101 @@
+package com.atu.index.keyword;
+
+import com.atu.index.IndexAware;
+import com.atu.utils.CommonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-29 10:44
+ * @description:
+ */
+@Slf4j
+@Component
+public class UnitKeywordIndex implements IndexAware<String, Set<Long>> {
+
+    private static Map<String, Set<Long>> keywordUnitMap;
+    private static Map<Long, Set<String>> unitKeywordMap;
+
+    static {
+        keywordUnitMap = new ConcurrentHashMap<>();
+        unitKeywordMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public Set<Long> get(String key) {
+        if (StringUtils.isEmpty(key)) {
+            return Collections.emptySet();
+        }
+        Set<Long> result = keywordUnitMap.get(key);
+        if (result == null) {
+            return Collections.emptySet();
+        }
+        return result;
+    }
+
+    @Override
+    public void add(String key, Set<Long> value) {
+        log.info("UnitKeywordIndex, before add: {}", unitKeywordMap);
+        //当keywordUnitMap不存在key时，会返回一个ConcurrentSkipListSet对象
+        Set<Long> unitIdSet = CommonUtils.getorCreate(key, keywordUnitMap, ConcurrentSkipListSet::new);
+        unitIdSet.addAll(value);
+        for (Long unitId : value) {
+            Set<String> keywordSet = CommonUtils.getorCreate(unitId, unitKeywordMap, ConcurrentSkipListSet::new);
+            keywordSet.add(key);
+        }
+        log.info("UnitKeywordIndex, after add: {}", unitKeywordMap);
+    }
+
+    /**
+     * 更新成本比较高，最好的实现方式就是去删除然后add
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void update(String key, Set<Long> value) {
+        log.error("keyword index can not support update");
+    }
+
+    @Override
+    public void delete(String key, Set<Long> value) {
+        log.info("UnitKeywordIndex, before delete: {}", unitKeywordMap);
+        Set<Long> unitIdS = CommonUtils.getorCreate(
+                key, keywordUnitMap, ConcurrentSkipListSet::new);
+        unitIdS.remove(value);
+
+        for (Long unitId : value) {
+            Set<String> keywordSet = CommonUtils.getorCreate(unitId, unitKeywordMap, ConcurrentSkipListSet::new);
+
+            keywordSet.remove(key);
+        }
+        log.info("UnitKeywordIndex, after delete: {}", unitKeywordMap);
+    }
+
+    /**
+     * 判断匹配
+     *
+     * @param unitId
+     * @param keywords
+     * @return
+     */
+    public boolean match(Long unitId, List<String> keywords) {
+        if (unitKeywordMap.containsKey(unitId) && CollectionUtils.isNotEmpty(unitKeywordMap.get(unitId))) {
+            Set<String> unitKeywords = unitKeywordMap.get(unitId);
+            return CollectionUtils.isSubCollection(keywords,unitKeywords);
+        }
+
+        return false;
+    }
+
+}
---
+++ application/ad-springcloud/ad-service/ad-search/src/main/java/com/atu/index/keyword/UnitKeywordObject.java
@@ -0,0 +1,18 @@
+package com.atu.index.keyword;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-29 10:44
+ * @description:
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitKeywordObject {
+    private Long unitId;
+    private String keyword;
+}
