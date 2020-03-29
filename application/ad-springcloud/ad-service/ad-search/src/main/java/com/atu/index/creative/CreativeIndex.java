d3c79ae7d711154cd4860309f508d3e2b16dcd4a
D812359 <287202628@qq.com>
2020-03-29 15:03:48 +0800

    广告检索系统-广告数据索引的设计与实现【难度在逐步上升】 6、创意索引对象定义与服务实现

---
+++ application/ad-springcloud/ad-service/ad-search/src/main/java/com/atu/index/creative/CreativeIndex.java
@@ -0,0 +1,56 @@
+package com.atu.index.creative;
+
+import com.atu.index.IndexAware;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author: Tom
+ * @date: 2020-03-29 14:57
+ * @description:
+ */
+@Slf4j
+@Component
+public class CreativeIndex implements IndexAware<Long, CreativeObject> {
+    //只有一个正向索引
+    private static Map<Long, CreativeObject> objectMap;
+
+    static {
+        objectMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public CreativeObject get(Long key) {
+        return objectMap.get(key);
+    }
+
+    @Override
+    public void add(Long key, CreativeObject value) {
+        log.info("before add: {}", objectMap);
+        objectMap.put(key, value);
+        log.info("after add: {}", objectMap);
+    }
+
+    @Override
+    public void update(Long key, CreativeObject value) {
+        log.info("before update: {}", objectMap);
+
+        CreativeObject oldObject = objectMap.get(key);
+        if (null == oldObject) {
+            objectMap.put(key, value);
+        } else {
+            oldObject.update(value);
+        }
+        log.info("after update: {}", objectMap);
+    }
+
+    @Override
+    public void delete(Long key, CreativeObject value) {
+        log.info("before delete: {}", objectMap);
+        objectMap.remove(key);
+        log.info("after delete: {}", objectMap);
+    }
+}
