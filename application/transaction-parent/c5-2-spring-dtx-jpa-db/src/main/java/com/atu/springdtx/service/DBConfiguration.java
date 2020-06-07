5b226dc0fd061e2ae0b65fedd03914ae7e7857ee
xushiyao <em1228@qq.com>
2020-06-07 20:24:19 +0800

    分布式事务实现，模式和技术 实例2-JPA-DB.链式事务管理器-下

--- application/transaction-parent/c5-2-spring-dtx-jpa-db/src/main/java/com/atu/springdtx/service/DBConfiguration.java
+++ application/transaction-parent/c5-2-spring-dtx-jpa-db/src/main/java/com/atu/springdtx/service/DBConfiguration.java
@@ -7,7 +7,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.transaction.ChainedTransactionManager;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -30,6 +36,40 @@ public class DBConfiguration {
     @Primary
     public DataSource userDataSource() {
         return userDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    /**
+     * 手动创建jpa的EntityManagerFactory
+     *
+     * @return
+     */
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        //不需要自动维护数据库
+        vendorAdapter.setGenerateDdl(false);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.atu");
+        factory.setDataSource(userDataSource());
+        return factory;
+    }
+
+    /**
+     * 分别创建两个数据库的 transactionManager
+     *
+     * @return
+     */
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager userTM = new JpaTransactionManager();
+        userTM.setEntityManagerFactory(entityManagerFactory().getObject());
+        PlatformTransactionManager orderTM = new DataSourceTransactionManager(orderDataSource());
+
+        //参数的位置对执行顺序是由影响的， 参数在前，提交在后
+        ChainedTransactionManager tm = new ChainedTransactionManager(orderTM, userTM);
+        return tm;
     }
 
     /**
