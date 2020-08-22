2f5bf34eaf5fd521852d97e9bbff9051d135fa0d
xushiyao <em1228@qq.com>
2020-08-22 11:23:22 +0800

    第3章 数据库 数据库架构 优化索引

---
+++ java-parent/java-senior/src/main/java/com/atu/senior/lambda/Test.java
@@ -0,0 +1,16 @@
+package com.atu.senior.lambda;
+
+/**
+ * @author: Tom
+ * @date: 2020-08-05 14:21
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
