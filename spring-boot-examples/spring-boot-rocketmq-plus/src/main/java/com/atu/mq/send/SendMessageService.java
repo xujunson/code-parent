0f52983c5bc1603ae406094205c5e0bdebef0be9
p-shiyao.xu <em1228@qq.com>
2020-12-18 16:35:56 +0800

    消息延时推送

--- spring-boot-examples/spring-boot-rocketmq-plus/src/main/java/com/atu/mq/send/SendMessageService.java
+++ spring-boot-examples/spring-boot-rocketmq-plus/src/main/java/com/atu/mq/send/SendMessageService.java
@@ -2,12 +2,17 @@ package com.atu.mq.send;
 
 import com.atu.mq.MQMessageChannel;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.common.message.MessageConst;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: Tom
@@ -23,7 +28,11 @@ public class SendMessageService {
 
     public void sendMessage(String message) {
         log.info("消息队列即将发送消息:[{}]", message);
-
-        messageChannel.send(MessageBuilder.withPayload(message).build());
+        //消息延迟 1-1s 2-5s 3-10s 4-30s
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(MessageConst.PROPERTY_TAGS, "tagStr");
+        headers.put(MessageConst.PROPERTY_DELAY_TIME_LEVEL, 4);
+        Message msg = MessageBuilder.createMessage(message, new MessageHeaders(headers));
+        messageChannel.send(msg);
     }
 }
