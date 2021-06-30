cc5278b722806c5a5192a0eca5ac29931c77c6f6
xujunson <em1228@qq.com>
2021-06-30 21:37:58 +0800

    两数之和

--- tom-code/src/main/java/com/atu/algorithm/TwoSum.java
+++ tom-code/src/main/java/com/atu/algorithm/TwoSum.java
@@ -7,9 +7,14 @@ import java.util.Map;
  * @Author: Tom
  * @Date: 2021/6/30 0:22
  * @Description: 两数之和
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * https://leetcode-cn.com/problems/two-sum/
  */
-public class Solution {
-    public int[] twoSum(int[] nums, int target) {
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        //方案一：暴力枚举
        /* int n = nums.length;
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
@@ -20,6 +25,7 @@ public class Solution {
         }
         return new int[0];*/
 
+        //方案二：哈希表
         Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; ++i) {
             if (hashtable.containsKey(target - nums[i])) {
@@ -29,5 +35,9 @@ public class Solution {
         }
         return new int[0];
     }
-
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,2,4};
+        System.out.println(twoSum(nums, 6)[0]);
+        System.out.println(twoSum(nums, 6)[1]);
+    }
 }
