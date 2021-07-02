d59cc2f13fafc8ff408f0e4ad31f7aa2f30a70ed
xushiyao <xushiyao@cheyipai.com>
2021-07-02 11:12:54 +0800

    webflux

---
+++ spring-boot-examples/webflux/webflux-helloworld/src/main/java/com/atu/WebFluxApplication.java
@@ -0,0 +1,16 @@
+package com.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Tom
+ * @Date: 2021/7/2 10:58 上午
+ * @Description:
+ */
+@SpringBootApplication
+public class WebFluxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxApplication.class, args);
+    }
+}
