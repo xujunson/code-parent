2d7605c45c17896aa4af9f35ee5627dbad97934f
xushiyao <xushiyao@cheyipai.com>
2021-07-05 11:27:39 +0800

    NIO

---
+++ java-parent/java-senior/src/main/java/com/atu/senior/io/NIOFileCopy.java
@@ -0,0 +1,52 @@
+package com.atu.senior.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @Author: Tom
+ * @Date: 2021/7/5 10:37 上午
+ * @Description:
+ */
+public class NIOFileCopy {
+
+    public static void main(String[] args) throws IOException {
+        // 设置输入源 & 输出地 = 文件
+        String infile = "/Users/xujunson/Downloads/bill_monitor_task.sql";
+        String outfile = "/Users/xujunson/Downloads/copy.txt";
+
+        // 1. 获取数据源 和 目标传输地的输入输出流（此处以数据源 = 文件为例）
+        FileInputStream fin = new FileInputStream(infile);
+        FileOutputStream fout = new FileOutputStream(outfile);
+
+        // 2. 获取数据源的输入输出通道
+        FileChannel fcin = fin.getChannel();
+        FileChannel fcout = fout.getChannel();
+
+        // 3. 创建缓冲区对象
+        ByteBuffer buff = ByteBuffer.allocate(1024);
+
+        while (true) {
+
+            // 4. 从通道读取数据 & 写入到缓冲区
+            // 注：若 以读取到该通道数据的末尾，则返回-1
+            int r = fcin.read(buff);
+            if (r == -1) {
+                break;
+            }
+            // 5. 传出数据准备：调用flip()方法
+            buff.flip();
+
+            // 6. 从 Buffer 中读取数据 & 传出数据到通道
+            fcout.write(buff);
+
+            // 7. 重置缓冲区
+            buff.clear();
+
+        }
+    }
+
+}
