fef2bfce5a5c9c67b760d941a7040edbbf6d510e
xushiyao <xushiyao@cheyipai.com>
2021-07-29 15:36:29 +0800

    Sentinel 接入控制台

---
+++ spring-boot-examples/spring-boot-sentinel/spring-boot-sentinel-demo/src/main/java/com/atu/config/FilterConfig.java
@@ -0,0 +1,31 @@
+/*
+package com.atu.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+*/
+/**
+ * @Author: Tom
+ * @Date: 2021/7/29 2:51 下午
+ * @Description:
+ *//*
+
+@Configuration
+class FilterConfig {
+   */
+/* @Bean
+    public FilterRegistrationBean sentinelFilterRegistration() {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new CommonFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("sentinelFilter");
+        registration.setOrder(1);
+        return registration;
+    }*//*
+
+}
+*/
