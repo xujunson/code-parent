8ff682b000b15a580dea1540e7ab7321ad282414
xushiyao <xushiyao@cheyipai.com>
2022-01-27 16:45:24 +0800

    springboot+nginx

--- spring-boot-examples/spring-boot-vue/src/main/java/com/atu/controller/HelloController.java
+++ spring-boot-examples/spring-boot-vue/src/main/java/com/atu/controller/HelloController.java
@@ -1,6 +1,31 @@
-package com.atu.controller;/**
+package com.atu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
  * @Author: Tom
  * @Date: 2022/1/27 10:54 上午
- * @Description: 
- */public class HelloController {
+ * @Description:
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("index")
+    public String index() {
+        return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/hello/{id}")
+    public Map get(@PathVariable int id) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        map.put("name", "test");
+        map.put("desc", "test desc");
+        return map;
+    }
 }
